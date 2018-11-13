4150ccf37b0420963281816ad0ba81d8f454e859
Aleksandar Ratesic <aratesic@gmail.com>
2018-11-13 19:43:24 +0100

    Update shiny-server.sh

--- shiny-3.5.0-r1/shiny-server.sh
+++ shiny-3.5.0-r1/shiny-server.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Make sure the directory for individual app logs exists
-mkdir -p /var/log/shiny-server
-chown shiny.shiny /var/log/shiny-server
+# Moved these instructions to Dockerfile for now:
+# mkdir -p /var/log/shiny-server
+# chown shiny:shiny /var/log/shiny-server
 
 exec /usr/bin/shiny-server 2>&1
